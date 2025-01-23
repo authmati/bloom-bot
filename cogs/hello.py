75b811e9917c9c447319930e8729848e0053e178
authmati <matiasayala517@gmail.com>
2025-01-23 13:30:06 -0300

    Modificando y optimizando un poco el hello.py

--- cogs/hello.py
+++ cogs/hello.py
@@ -1,35 +1,41 @@
 # Importar librerias necesarias:
-import discord
-from discord import app_commands
 from discord.ext import commands
 import random
 
+
 # Iniciar cog de saludo:
-class hello(commands.Cog):
+class Hello(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{__name__} está listo.")
 
-# Mensajes predeterminados al etiquetar al bot:
+    # Mensajes predeterminados al etiquetar al bot:
     @commands.Cog.listener()
     async def on_message(self, message):
-        if self.client.user in message.mentions:
-                    responses = [
-            f"Robando una vez más 💵, {message.author.mention}!",
-            f"Casha 🤬, {message.author.mention}!",
-            f"Deja de molestarme, {message.author.mention}... 😒"
-        ]
-        response = random.choice(responses)
-        await message.channel.send(response)
-        await self.client.process_commands(message)
+        if message.author.bot:
+            return
 
-# Comando para que el bot te salude:
+        if self.client.user in message.mentions:
+
+            RESPONSES = [
+                "Robando una vez más 💵, {mention}!",
+                "Casha 🤬, {mention}!",
+                "Deja de molestarme, {mention}... 😒",
+                "Que pasa pibe!",
+            ]
+
+            response = random.choice(RESPONSES).format(mention=message.author.mention)
+            await message.channel.send(response)
+            await self.client.process_commands(message)
+
+    # Comando para que el bot te salude:
     @commands.hybrid_command(name="saludar", description="Saluda al usuario.")
     async def saludar(self, ctx):
-         await ctx.send(f"Hola, {ctx.author.mention} pasa los cogollos!")
+        await ctx.send(f"Hola, {ctx.author.mention} pasa los cogollos!")
+
 
 async def setup(client):
-    await client.add_cog(hello(client))
+    await client.add_cog(Hello(client))
